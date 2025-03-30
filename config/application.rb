48954bcf8ee2584bc78742b4d567f85e0051c1f1
hatanatsu1993 <086.yga4jjguaf5@gmail.com>
2025-03-30 11:31:15 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module Protospace41902
+  class Application < Rails::Application
+    config.load_defaults 7.1
+    
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    config.active_storage.variant_processor = :mini_magick
+    
+  end
+end
